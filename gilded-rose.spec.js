aae6236a0ad3d8cc5e6a86857ff822085d8b79e4
Drew Kullman <agkullman@protonmail.com>
2023-12-08 03:17:50 -0900

    Commit 7 - All Test Cases Written

--- gilded-rose.spec.js
+++ gilded-rose.spec.js
@@ -2,30 +2,25 @@ import { expect, describe, it } from "vitest";
 import { BackstagePass, Conjured, Cheese, Legendary, Item, items, updateQuality, updateItem, updateLegendary, updateCheese, updateConjured, updateBackstagePass} from "./gilded-rose.js";
 
 describe("updateQuality", () => {
-  it("reduces quality and sellIn of basic items by 1", () => {
-    
+  it("executes specifics updates based on that item's subclass", () => { 
 
-    const testItem = new Item("basic", 5, 3);
-    const testItem2 = new Conjured("Conjured Sword of Doom", 3, 6 );
-    const testItem3 = new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 11);
-    const testItem4 = new Legendary("Sulfuras, Hand of Ragnaros", 0, 80);
-    const testItem5 = new Cheese("Aged Brie", 2, 0);
-    items.push(testItem, testItem2, testItem3, testItem4, testItem5);
-
+    const testBasic = new Item("basic", 5, 3);
+    const testConjured = new Conjured("Conjured Sword of Doom", 3, 6 );
+    const testBackstagePass = new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 11);
+    const testLegendary = new Legendary("Sulfuras, Hand of Ragnaros", 0, 80);
+    const testCheese = new Cheese("Aged Brie", 2, 0);
+    items.push(testBasic, testConjured, testBackstagePass, testLegendary, testCheese);
     updateQuality();
-
-    expect(testItem.quality).toBe(2);
-    expect(testItem.sellIn).toBe(4);
-
-    expect(testItem2.quality).toBe(4);
-    expect(testItem2.sellIn).toBe(2);
-
-    expect(testItem3.quality).toBe(13);
-    expect(testItem3.sellIn).toBe(9);
-    expect(testItem4.quality).toBe(80);
-    expect(testItem4.sellIn).toBe(0)
-    expect(testItem5.quality).toBe(1);
-    expect(testItem5.sellIn).toBe(1)
+    expect(testBasic.quality).toBe(2);
+    expect(testBasic.sellIn).toBe(4);
+    expect(testConjured.quality).toBe(4);
+    expect(testConjured.sellIn).toBe(2);
+    expect(testBackstagePass.quality).toBe(13);
+    expect(testBackstagePass.sellIn).toBe(9);
+    expect(testLegendary.quality).toBe(80);
+    expect(testLegendary.sellIn).toBe(0);
+    expect(testCheese.quality).toBe(1);
+    expect(testCheese.sellIn).toBe(1);
   });
 });
 
@@ -40,3 +35,38 @@ describe("updateItem", () => {
   })
 });
 
+describe("updateLegendary", () => {
+  it ("As 'Legendary' item, does not reduce quality and leaves the sell by date to default", () => {
+    const item = new Legendary("Sulfuras, Hand of Ragnaros", 0, 80);
+    updateLegendary(item);
+    expect(item.quality).toBe(80);
+    expect(item.sellIn).toBe(0)
+  })
+})
+
+describe("updateCheese", () => {
+  it("As sellIn decreases by 1 day, quality increases by 1. After sellIn is less than 0, quality then decreases by 1.", () => {
+    const item = new Cheese("Aged Brie", 2, 0);
+    updateCheese(item);
+    expect(item.quality).toBe(1);
+    expect(item.sellIn).toBe(1);
+  })
+})
+
+describe("updateConjured", () => {
+  it("As sellIn decreases by 1, the quality decreases by 2. After sellIn is less than 0, quality decreases by 4", () => {
+    const item = new Conjured("Conjured Sword of Doom", 3, 6 );
+    updateConjured(item);
+    expect(item.quality).toBe(4);
+    expect(item.sellIn).toBe(2);
+  })
+})
+
+describe("updateBackstagePass", ()=> {
+  it("When item is between 10 and 5 days, value increases by 2. When item is between 5 and 0 days, value increase by 3.", () => {
+    const item = new BackstagePass("Backstage passes to a TAFKAL80ETC concert", 10, 11);
+    updateBackstagePass(item);
+    expect(item.quality).toBe(13);
+    expect(item.sellIn).toBe(9);
+  })
+})
